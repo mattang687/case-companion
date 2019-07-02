2d9e20a58af142e5c194d05de267fd4961e63cea
Matt Tang <mattang687@gmail.com>
2019-07-02 12:58:33 -0400

    implemented secondary humidity axis

--- lib/screens/home_page/graph_widgets.dart
+++ lib/screens/home_page/graph_widgets.dart
@@ -17,7 +17,16 @@ class TempHumChart extends StatelessWidget {
 
   @override
   Widget build (BuildContext context) {
-    return new charts.LineChart(seriesList, animate: animate,);
+    return new charts.LineChart(
+      seriesList, 
+      animate: animate,
+      primaryMeasureAxis: new charts.NumericAxisSpec(
+        tickProviderSpec: new charts.BasicNumericTickProviderSpec(desiredTickCount: 3),
+      ),
+      secondaryMeasureAxis: new charts.NumericAxisSpec(
+        tickProviderSpec: new charts.BasicNumericTickProviderSpec(desiredTickCount: 3),
+      ),
+    );
   }
 
   static List<charts.Series<Entry, int>> _createSampleData() {
@@ -25,33 +34,40 @@ class TempHumChart extends StatelessWidget {
       new Entry(
         time: 0,
         temp: 54,
-        hum: 36
+        hum: 1000
       ),
       new Entry(
         time: 1,
         temp: 23,
-        hum: 67
+        hum: 2000
       ),
       new Entry(
         time: 2,
         temp: 82,
-        hum: 45
+        hum: 3000,
       ),
       new Entry(
         time: 3,
         temp: 32,
-        hum: 54
+        hum: 4000,
       ),
     ];
 
     return [
       new charts.Series<Entry, int>(
-        id: 'data',
+        id: 'Temperature',
         colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
         domainFn: (Entry e, _) => e.time,
         measureFn:  (Entry e, _) => e.temp,
         data: data,
-      )
+      ),
+      new charts.Series<Entry, int>(
+        id: 'Humidity',
+        colorFn: (_, __) => charts.MaterialPalette.deepOrange.shadeDefault,
+        domainFn: (Entry e, _) => e.time,
+        measureFn: (Entry e, _) => e.hum,
+        data: data,
+      )..setAttribute(charts.measureAxisIdKey, 'secondaryMeasureAxisId')
     ];
   }
 }
