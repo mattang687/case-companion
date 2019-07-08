54768bd703c1ac4a288f44bf2c912349b1188996
Matt Tang <mattang687@gmail.com>
2019-07-08 14:07:39 -0400

    lock orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:myapp/local_data/database_helper.dart';
 import 'package:myapp/local_data/settings_helper.dart';
 import 'package:provider/provider.dart';
@@ -7,15 +8,18 @@ import 'bluetooth/inherited_bluetooth.dart';
 import 'screens/home_page/home_page.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider<InheritedBluetooth>.value(
-            value: InheritedBluetooth()),
-        ChangeNotifierProvider<SettingsHelper>.value(value: SettingsHelper()),
-        ChangeNotifierProvider<DatabaseHelper>.value(value: DatabaseHelper()),
-      ],
-      child: MaterialApp(title: "Case Companion", home: HomePage()),
-    ),
-  );
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<InheritedBluetooth>.value(
+              value: InheritedBluetooth()),
+          ChangeNotifierProvider<SettingsHelper>.value(value: SettingsHelper()),
+          ChangeNotifierProvider<DatabaseHelper>.value(value: DatabaseHelper()),
+        ],
+        child: MaterialApp(title: "Case Companion", home: HomePage()),
+      ),
+    );
+  });
 }
